10f49976fe4d626bd0cd094f6194357a3d0e4fa1
molodec08 <molodec.08@mail.ru>
2021-01-18 14:12:02 +0300

    lesson4 переход на vue

--- app/src/main.js
+++ app/src/main.js
@@ -1,71 +1,54 @@
-import '../scss/style.scss'
+import '../scss/style.scss';
 
-const addPage = document.getElementsByClassName('add');
-
-function getPagesList() {
-    const allBlock = document.querySelectorAll('div');
-    if (allBlock !== null) {
-        allBlock.forEach( e => e.remove() );
+const App = {
+    data() {
+        return {
+            pageList: [],
+            newPageName: ''
+        }        
+    },
+    created() {
+            this.updatePageList();
+    },
+    methods:{
+        createPage() {
+            const test = new FormData();
+            test.set( "name", this.newPageName );
+            fetch('./api/createNewHTMLPage.php', {
+                method: 'POST',
+                body: test
+            })
+            .then((response) => {
+                if (!response.ok) {
+                    // Сервер вернул код ответа за границами диапазона [200, 299]
+                    alert("Ошибка! Такая страница уже существует!")
+                } else {
+                    this.updatePageList();
+                } 
+            })
+        },
+        updatePageList() {
+            fetch("./api", {method: 'get'})
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.pageList = data;
+            })
+        },
+        deleteBlock(page) {
+            const test = new FormData();
+            test.set( "name", page );
+            fetch('./api/deleteHTMLPage.php', {
+                method: 'POST',
+                body: test
+            })
+            .then((response) => {
+                this.updatePageList();        
+            })
+        }
     }
-
-
-    fetch("./api", {method: 'get'})
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            data.forEach((file) => {
-                const div = document.createElement('div');
-                div.classList.add('block');
-                div.innerHTML = `<h1>${file}</h1><button class="deleteButton" type="button" id=${file}>Удалить</button>`;
-                document.querySelector('body').append(div);
-                
-            }); 
-
-            const deleteBlock = document.querySelectorAll('.deleteButton');
-                console.log(deleteBlock); 
-                
-            deleteBlock.forEach(e => e.addEventListener('click', () => {
-                const test = new FormData();
-                test.set( "name", e.id );
-                fetch('./api/deleteHTMLPage.php', {
-                    method: 'POST',
-                    body: test
-                })
-                .then((response) => {
-                    console.log(response.text());
-                    if (!response.ok) {
-                        // Сервер вернул код ответа за границами диапазона [200, 299]
-                        alert("Ошибка! Такая страница уже существует!")
-                    } else {
-                        getPagesList();
-                    }        
-                })
-            }))
-        });
-
-    
-
 }
 
-getPagesList();
-
-addPage[0].addEventListener('click', () => {
-    const test = new FormData();
-    test.set( "name", document.querySelector('input').value );
-    
-    fetch('./api/createNewHTMLPage.php', {
-        method: 'POST',
-        body: test
-    })
-    .then((response) => {
-        if (!response.ok) {
-            // Сервер вернул код ответа за границами диапазона [200, 299]
-            alert("Ошибка! Такая страница уже существует!")
-        } else {
-            getPagesList();
-        }        
-    })
-    // .catch(() => alert("Ошибка! Такая страница уже существует!"))
-});
+Vue.createApp(App).mount('#app');
 
